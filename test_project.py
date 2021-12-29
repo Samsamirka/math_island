a797224820437f717f8b4716c2f9f3805f20184b
Samira Amirova <miss-x-sam@yandex.ru>
2021-12-29 18:43:52 +0300

    первый скелет порграммы

--- test_project.py
+++ test_project.py
@@ -18,7 +18,7 @@ class Island:
     def __init__(self):
         pass
 
-    def theory_open(self):
+    def theory_open(self):  # теория везде одинаковая
         pass
 
     def control_work_open(self):
@@ -38,3 +38,151 @@ class Island:
 
     def exercise_5_open(self):
         pass
+
+
+def condition_open(self):  # эта функция повторяется в каждых классах заданий
+    pass
+
+
+def render_answer(self):  # ответ от системы(верный/неверный ответ)
+    pass
+
+
+class LengthSpoke:  # первое задание (spoke - спица)
+    def __init__(self):
+        pass
+
+    def render_exercise_1(self):
+        pass
+
+    def check_answer_1(self):
+        pass
+
+    def get_answer_1(self):
+        pass
+
+
+class SurfaceArea:  # второе задание
+    def __init__(self):
+        pass
+
+    def render_exercise_2(self):
+        pass
+
+    def check_answer_2(self):
+        pass
+
+    def get_answer_2(self):
+        pass
+
+
+class Radius:  # третье задание
+    def __init__(self):
+        pass
+
+    def render_exercise_3(self):
+        pass
+
+    def check_answer_3(self):
+        pass
+
+    def get_answer_3(self):
+        pass
+
+
+class AreaCupola:  # четвертое задание
+    def __init__(self):
+        pass
+
+    def render_exercise_4(self):
+        pass
+
+    def check_answer_4(self):
+        pass
+
+    def get_answer_4(self):
+        pass
+
+
+class PercentFabrics:  # пятое задание
+    def __init__(self):
+        pass
+
+    def render_exercise_5(self):
+        pass
+
+    def check_answer_5(self):
+        pass
+
+    def get_answer_5(self):
+        pass
+
+
+class ControlWork:
+    def __init__(self):
+        pass
+
+    class LengthSpokeControl:  # первое задание (spoke - спица)
+        def __init__(self):
+            pass
+
+        def render_exercise_control_1(self):
+            pass
+
+        def check_answer_control_1(self):
+            pass
+
+        def get_answer_control_1(self):
+            pass
+
+    class SurfaceAreaControl:  # второе задание
+        def __init__(self):
+            pass
+
+        def render_exercise_control_2(self):
+            pass
+
+        def check_answer_control_2(self):
+            pass
+
+        def get_answer_control_2(self):
+            pass
+
+    class RadiusControl:  # третье задание
+        def __init__(self):
+            pass
+
+        def render_exercise_control_3(self):
+            pass
+
+        def check_answer_control_3(self):
+            pass
+
+        def get_answer_control_3(self):
+            pass
+
+    class AreaCupolaControl:  # четвертое задание
+        def __init__(self):
+            pass
+
+        def render_exercise_control_4(self):
+            pass
+
+        def check_answer_control_4(self):
+            pass
+
+        def get_answer_control_4(self):
+            pass
+
+    class PercentFabrics:  # пятое задание
+        def __init__(self):
+            pass
+
+        def render_exercise_control_5(self):
+            pass
+
+        def check_answer_control_5(self):
+            pass
+
+        def get_answer_control_5(self):
+            pass
