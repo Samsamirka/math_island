4c97410493f40057f1d201dbc632436f43247149
gallelizabeth <liza.a.gall@gmail.com>
2022-01-22 23:44:20 +0300

    добавила музыку, но она не останавливается

--- menu_window.py
+++ menu_window.py
@@ -21,8 +21,8 @@ screen = pygame.transform.scale(screen, (screen.get_width()//1.6, screen.get_hei
 manager = pygame_gui.UIManager((HEIGHT, WIDTH))
 
 
-# pygame.mixer.music.load('data/')  # добавить музыку
-# pygame.mixer.music.play(-1, 0.0)
+pygame.mixer.music.load('data/music.mp3')
+pygame.mixer.music.play(-1, 0.0)
 music_playing = True
 
 all_btns = pygame.sprite.Group()
