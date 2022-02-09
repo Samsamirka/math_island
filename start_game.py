5881be486f61958f5426e5a463841aebf58615b7
kotik <90791523+gallelizabeth@users.noreply.github.com>
2022-02-09 19:27:40 +0300

    исправление текста и бд

--- start_game.py
+++ start_game.py
@@ -1,16 +1,16 @@
 import pygame
+import sqlite3
 
 HEIGHT, WIDTH = 1500, 937
 COLOR_ACTIVE = pygame.Color('white')
 COLOR_INACTIVE = pygame.Color((30, 30, 30))
-FONT = pygame.font.Font(None, 32)
 
+input_text = ''
 window_surface = pygame.display.set_mode((HEIGHT, WIDTH))
 # screen = pygame.Surface((HEIGHT, WIDTH))
 # screen.fill(pygame.Color('black'))
+
 screen = pygame.image.load('data/new_fon.png')
-
-
 all_sprites = pygame.sprite.Group()
 
 
@@ -22,41 +22,33 @@ class Continued_game(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(515, 350))
 
     def update(self, *args):
-
         if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
-            import sqlite3
-
-            con = sqlite3.connect("users.db")
-
+            con = sqlite3.connect("data/users.db")
             cur = con.cursor()
-
-            users_id = cur.execute("""SELECT * FROM users MAX(id)""")
-            result = cur.execute("""SELECT * FROM users WHERE id == """ + str(id) + '"""')
-
+            users_id = cur.execute("SELECT * FROM users MAX(id)")
+            result = cur.execute("""SELECT *
+                                    FROM users
+                                    WHERE id = ?""", users_id)
             con.close()
 
 
 class New_game(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(all_sprites)
-        self.text = None
         self.image = pygame.image.load('data/new_game.png')
         self.image = pygame.transform.scale(self.image, (self.image.get_width() // 2, self.image.get_height() // 2))
         self.rect = self.image.get_rect(center=(500, 600))
 
-    def r_text(self, txt):
-        self.text = txt
-
     def update(self, *args):
+        global input_text
         if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
-            import sqlite3
-
-            con = sqlite3.connect("users.db")
-
+            con = sqlite3.connect("data/users.db")
             cur = con.cursor()
-
             delt = """DELETE FROM users"""
-            new_profil = cur.execute("""INSERT INTO users.names(""" + self.text + '"""')
+            new_profil = cur.execute("""INSERT INTO users(names) VALUES(?) RETURNING id""", (input_text,)).fetchone()[0]
+            con.commit()
+            print(new_profil)
+            con.close()
 
 
 cont = Continued_game()
@@ -64,7 +56,7 @@ play = New_game()
 
 
 def start(clock: pygame.time.Clock):
-    input_text = ''
+    global input_text
     running = True
     while running:
         for event in pygame.event.get():
@@ -79,23 +71,19 @@ def start(clock: pygame.time.Clock):
                                                                from_player.get_height()))
                     screen.blit(from_player, (800, 310))
                 elif event.key == pygame.K_RETURN:
-                    New_game().r_text(input_text)
+                    play.update()
                 else:
                     input_text += event.unicode
                     font = pygame.font.Font('data/ofont.ru_AsylbekM29.kz.ttf', 45)
                     from_player = font.render(input_text, True, (0, 0, 0))
                     screen.blit(from_player, (800, 310))
-
             pygame.display.flip()
             if event.type == pygame.QUIT:
                 running = False
             all_sprites.update(event)
         window_surface.blit(screen, (0, 0))
-
         screen.blit(cont.image, cont.rect)
         screen.blit(play.image, play.rect)
-
         pygame.display.update()
         clock.tick(30)
-
     return
